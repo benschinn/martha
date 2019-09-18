93ba52f919c32e6b0fae65a52895c1c97bfc1cd4
Ben Schinn <ben.schinn@mx.com>
2019-09-17 23:32:22 -0600

    grabbing title

--- lib/martha_web/controllers/page_controller.ex
+++ lib/martha_web/controllers/page_controller.ex
@@ -8,9 +8,9 @@ defmodule MarthaWeb.PageController do
   def get_recipe() do
     case HTTPoison.get("https://www.allrecipes.com/recipe/14231/guacamole/") do
       {:ok, %HTTPoison.Response{status_code: 200, body: body}} -> 
-        node = Floki.find(body, "h1#recipe-main-content")
-        text = Floki.text(node)
-        %{title: text}
+        node = Floki.find(body, "h1")
+        title = Floki.text(node)
+        %{title: title}
       {:ok, %HTTPoison.Response{status_code: 404}} -> IO.puts "404: Not Found"
       {:error, %HTTPoison.Error{reason: reason}} -> IO.inspect reason
     end
